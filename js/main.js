0aa87cbba61f051573eb511a0f1f16b678bdaed3
maribel <maribel.diaz.j@gmail.com>
2017-05-12 13:17:01 -0300

    cambios ejercicio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+var clientes = document.getElementById("clientes");
+
+
+var infoclientes = (“<b> Nombre : </b>” + nombre + “<br>” + “<b> Apellido : </b>” + apellido + “<br>” + “<b> Email : </b>” +
+    email + “<br>” + “<b> Dirección : </b>” + direccion + “<br>” + “<b> Teléfono : </b>” + telefono);
+
+clientes.innerHTML = (infoclientes);
+
+
+
+
+
+
